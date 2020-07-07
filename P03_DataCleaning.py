9b37e3e3f1389a8e846ece49438e92afc1e4b675
Chi Lam <theonlyasian.dc@gmail.com>
2020-07-06 23:19:38 -0400

    edited data cleaning

--- P03_DataCleaning.py
+++ P03_DataCleaning.py
@@ -8,13 +8,16 @@ Created on Sun Jul  5 14:52:01 2020
 #Import modules
 import pandas as pd
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import re
 from nltk.tag import pos_tag
 from nltk.corpus import wordnet
 from nltk.stem.wordnet import WordNetLemmatizer
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 #Read in data
 df = pd.read_csv('review_scraped.csv')
@@ -25,28 +28,28 @@ df['review_txt'] = df.review_header + ' ' + df.review_body
 
 
 #Cleaning text/ text preprocessing and text normalization
-def get_wordnet_pos(treebank_tag):
-    if treebank_tag.startswith('N'):
+def get_wordnet_pos(tag):
+    if tag.startswith('N'):
         return wordnet.NOUN
-    elif treebank_tag.startswith('V'):
+    elif tag.startswith('V'):
         return wordnet.VERB
-    elif treebank_tag.startswith('J'):
+    elif tag.startswith('J'):
         return wordnet.ADJ
-    elif treebank_tag.startswith('R'):
+    elif tag.startswith('R'):
         return wordnet.ADV
     else:
         return wordnet.NOUN
 
-def decontraction(text):
-    text = re.sub(r"n\'t", " not", text)
-    text = re.sub(r"\'re", " are", text)
-    text = re.sub(r"\'s", " is", text)
-    text = re.sub(r"\'d", " would", text)
-    text = re.sub(r"\'ll", " will", text)
-    text = re.sub(r"\'t", " not", text)
-    text = re.sub(r"\'ve", " have", text)
-    text = re.sub(r"\'m", " am", text)
-    return text
+def decontraction(text_string):
+    text_string = re.sub(r"n\'t", " not", text_string)
+    text_string = re.sub(r"\'re", " are", text_string)
+    text_string = re.sub(r"\'s", " is", text_string)
+    text_string = re.sub(r"\'d", " would", text_string)
+    text_string = re.sub(r"\'ll", " will", text_string)
+    text_string = re.sub(r"\'t", " not", text_string)
+    text_string = re.sub(r"\'ve", " have", text_string)
+    text_string = re.sub(r"\'m", " am", text_string)
+    return text_string
 
 def clean_text(text):
     text = text.lower() # convert text to lowercase
@@ -68,8 +71,17 @@ def clean_text(text):
     return text
     
 cleaned_text = df.review_txt.apply(lambda x: clean_text(x))
-df['cleaned_text'] = cleaned_text
+df['review_cleaned'] = cleaned_text
 
-df.cleaned_text[0]
-#n-grams
 
+#Split data to test and training set
+
+
+#Vectorization with n-gram
+ngram_cv = CountVectorizer(binary=True, ngram_range=(1, 3))
+ngram_cv.fit(df.review_cleaned)
+
+
+#Feature engineering: sentiment analysis
+
+                           
